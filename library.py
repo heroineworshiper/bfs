36162cd233fb467954a76046cc02adcc5efc0be6
Your Name <you@example.com>
2018-07-21 15:28:18 -0700

    x

--- library.py
+++ library.py
@@ -1,37 +1,76 @@
 # importing libraries doesn't work in freecad scripts
 
 
-
 import FreeCAD
+import FreeCADGui
 import Mesh
 import MeshPart
 
 
 
+doc = FreeCAD.ActiveDocument
 
-def makePart(dstName, srcObjects, flipNormals):
-    doc = FreeCAD.activeDocument()
-    guiDoc = FreeCAD.Gui.activeDocument()
+
+
+def isPart(x):
+    return str(type(x)) == '<type \'App.Part\'>'
+
+# convert an App.Part to a mesh
+def makePartPart(dsts, transform, part, doubleSided, flipNormals):
+    transform = transform * part.Placement.toMatrix()
+    for src in part.Group:
+        if src.ViewObject.Visibility:
+            if isPart(src):
+                makePartPart(dsts, transform, src, doubleSided, flipNormals)
+            else:
+                makePolyPart(dsts, transform, src, doubleSided, flipNormals)
+    #src.ViewObject.Visibility=False
+
+
+
+# convert a polygon to a mesh
+def makePolyPart(dsts, transform, src, doubleSided, flipNormals):
+    #print "got it"
+    mesh = doc.addObject("Mesh::Feature","Mesh")
+    shape = src.Shape
+    #mesh.Mesh = MeshPart.meshFromShape(Shape=shape,MaxLength=250.8)
+    mesh.Mesh = MeshPart.meshFromShape(Shape=shape,LinearDeflection=10, AngularDeflection=0.523599, Relative=False)
+
+    if src.Label in flipNormals:
+        mesh.Mesh.flipNormals()
+    if src.Label in doubleSided:
+        mesh.ViewObject.Lighting = u"Two side"
+        
+    mesh.Label = "%s Mesh" % src.Label
+    mesh.ViewObject.CreaseAngle=25.0
+    mesh.Placement = App.Placement(transform)
+    dsts.append(mesh)
+    src.ViewObject.Visibility=False
+
+
+
+
+
+def makePart(dstName, objects, doubleSided, flipNormals):
     dsts = []
-    for i in srcObjects:
-        mesh = doc.addObject("Mesh::Feature","Mesh")
-        src = doc.getObject(i)
-        shape = src.Shape
-        #mesh.Mesh = MeshPart.meshFromShape(Shape=shape,MaxLength=250.8)
-        mesh.Mesh = MeshPart.meshFromShape(Shape=shape,LinearDeflection=10, AngularDeflection=0.523599, Relative=False)
+    for src in doc.Objects:
+        if src.Label in objects:
+            transform = App.Matrix()
+            print 'label=%s name=%s' % (src.Label, src.Name)
+            if isPart(src):
+                makePartPart(dsts, transform, src, doubleSided, flipNormals)
+            else:
+                makePolyPart(dsts, transform, src, doubleSided, flipNormals)
+                
+                
+        
 
-        if i in flipNormals:
-            mesh.Mesh.flipNormals()
-        mesh.Label = "%s Mesh" % i
-        mesh.ViewObject.CreaseAngle=25.0
-        dsts.append(mesh)
-        guiDoc.getObject(i).Visibility=False
-
-    part = FreeCAD.App.activeDocument().addObject('App::Part','Part')
+    part = App.activeDocument().addObject('App::Part','Part')
     part.Label = dstName
     for i in dsts:
         part.addObject(i)
     return part
+
 
 
 
